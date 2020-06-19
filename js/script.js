e94997879ca9515a9a636e36bd932c9b3c6be9d3
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-06-19 18:21:19 +0200

    texts and projects margin

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
-AOS.init({
-  duration: 1200,
-})
-let daniel = document.getElementById('daniel')
-let developer = document.getElementById('developer')
-let title = document.getElementsByClassName('title')[1]
-let arrow = document.getElementById('arrow')
 
-setTimeout(function(){
-daniel.style.border = 'none'
-developer.innerHTML = "I'm a fullstack developer"
-developer.classList.add('title')
-developer.classList.add('bordered')
-developer.style.width = '25ch'
-developer.style.animation = 'type 1.5s steps(25)'
-console.log(developer.style.borderRight)
-
-}, 1700);
-
-setTimeout(function () {
-  setInterval(function() {
-    if (developer.classList.contains('bordered')) {
-      developer.classList.remove('bordered')
-    } else {
-      developer.classList.add('bordered')
-      
-    }
-  }, 600);
-}, 3400)
-
-
-
+  AOS.init({
+    duration: 1200,
+  })
+  let daniel = document.getElementById('daniel')
+  let developer = document.getElementById('developer')
+  let title = document.getElementsByClassName('title')[1]
+  let arrow = document.getElementById('arrow')
+  
+  setTimeout(function(){
+  daniel.style.border = 'none'
+  developer.innerHTML = "I'm a fullstack developer"
+  developer.classList.add('title')
+  developer.classList.add('bordered')
+  developer.style.width = '25ch'
+  developer.style.animation = 'type 1.5s steps(25)'
+  console.log(developer.style.borderRight)
+  
+  }, 1700);
+  
+  setTimeout(function () {
+    setInterval(function() {
+      if (developer.classList.contains('bordered')) {
+        developer.classList.remove('bordered')
+      } else {
+        developer.classList.add('bordered')
+        
+      }
+    }, 600);
+  }, 3400)  
