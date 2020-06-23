cd83b1eeadccb8b4d3121078ef915477107b5cca
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-06-23 17:00:17 +0200

    add description on site images

--- js/script.js
+++ js/script.js
@@ -1,31 +1,45 @@
+let log = console.log
+AOS.init({
+  duration: 1200,
+  disable: 'mobile'
+})
+let daniel = document.getElementById('daniel')
+let developer = document.getElementById('developer')
+let title = document.getElementsByClassName('title')[1]
+let arrow = document.getElementById('arrow')
 
-
-  AOS.init({
-    duration: 1200,
-    disable: 'mobile'
-  })
-  let daniel = document.getElementById('daniel')
-  let developer = document.getElementById('developer')
-  let title = document.getElementsByClassName('title')[1]
-  let arrow = document.getElementById('arrow')
-  
-  setTimeout(function(){
+setTimeout(function () {
   daniel.style.border = 'none'
   developer.innerHTML = "I'm a fullstack developer"
   developer.classList.add('title')
   developer.classList.add('bordered')
   developer.style.width = '25ch'
   developer.style.animation = 'type 1.5s steps(25)'
-  
-  }, 1700);
-  
-  setTimeout(function () {
-    setInterval(function() {
-      if (developer.classList.contains('bordered')) {
-        developer.classList.remove('bordered')
-      } else {
-        developer.classList.add('bordered')
-        
-      }
-    }, 600);
-  }, 3400)  
+
+}, 1700);
+
+setTimeout(function () {
+  setInterval(function () {
+    if (developer.classList.contains('bordered')) {
+      developer.classList.remove('bordered')
+    } else {
+      developer.classList.add('bordered')
+
+    }
+  }, 600);
+}, 3400)
+
+let sites = document.querySelectorAll('.site')
+
+sites.forEach(function(e){
+  e.addEventListener('mouseover', function() {
+    e.getElementsByClassName('hidden')[0].classList.add('description')
+    e.getElementsByClassName('hidden')[0].classList.remove('hidden')
+    
+    
+  })
+  e.addEventListener('mouseout', function() {
+    e.getElementsByClassName('description')[0].classList.add('hidden')
+    e.getElementsByClassName('description')[0].classList.remove('description')
+  })
+})
