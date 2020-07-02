675596c085f6927c2a5a0d32e75951b36b2fd359
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-07-02 19:20:22 +0200

    scroll position for project and skill nav

--- js/script.js
+++ js/script.js
@@ -43,3 +43,27 @@ sites.forEach(function (e) {
     e.getElementsByClassName('description')[0].classList.remove('description')
   })
 })
+
+
+window.addEventListener("scroll", function (event) {
+  var scroll = this.scrollY;
+  log(scroll)
+  if (scroll > 700 && scroll < 1850) {
+    document.getElementById('project').style.borderBottom = '1px solid white'
+  }
+  if (scroll > 1850 || scroll < 700) {
+    document.getElementById('project').style.borderBottom = 'none'
+  }
+  if (scroll > 1850 && scroll < 2184 ) {
+    document.getElementById('skills').style.borderBottom = '1px solid white'
+  }
+  if (scroll < 1850 || scroll > 2184) {
+    document.getElementById('skills').style.borderBottom = 'none'
+  }
+
+  
+});
+
+// document.getElementById('skills').addEventListener('click', function(){
+//   document.getElementById('skills').style.borderBottom = '1px solid white'
+// })
